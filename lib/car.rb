acd8f4f1f50e1fec98298e3a89daeff8bbf4539b
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-16 12:04:54 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
+ # Notice that we are requiring lib/vehicle.rb.
+ # That is because our Car class will need access to the Vehicle class and will therefore need access to the file that contains that class.
+
 require_relative "./vehicle.rb"
+
+class Car < Vehicle # SubClass < SuperClass . Subclass will inherit from SuperClass
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
